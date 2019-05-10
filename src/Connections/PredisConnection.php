001262981b02f72039433f832c56f116b0b9c211
Chu Xiang <chuxiang@Vchangyi.com>
2019-05-10 18:14:11 +0800

    将 Predis 驱动的 get mget hexists 方法返回值与 Phpredis 驱动保持一致

--- src/Connections/PredisConnection.php
+++ src/Connections/PredisConnection.php
@@ -23,6 +23,40 @@ class PredisConnection extends BaseConnection implements ConnectionContract
     }
 
     /**
+     * 返回字符串
+     *
+     * @param string $key
+     * @return false | string
+     */
+    public function get($key)
+    {
+        $result = $this->command('get', [$key]);
+        return is_null($result) ? false : $result;
+    }
+
+    /**
+     * 返回所查询键的值
+     *
+     * @param array $keys
+     * @return array
+     */
+    public function mget(array $keys)
+    {
+        return array_map(function($value){
+            return is_null($value) ? false : $value;
+        }, $this->command('mget', $keys));
+    }
+
+    /**
+     * @param dynamic $keys
+     * @return bool
+     */
+    public function hexists(...$keys)
+    {
+        return (bool)$this->command('hexists', $keys);
+    }
+
+    /**
      * 运行命令在 Redis Server 上
      *
      * @param string $method
